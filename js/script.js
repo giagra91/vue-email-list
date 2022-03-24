07c35e2ac9a71bf8c3c0f26435b42536939588d3
giagra91 <giagra91@gmail.com>
2022-03-24 16:00:50 +0100

    completamento esercizio

--- js/script.js
+++ js/script.js
@@ -3,17 +3,20 @@ let app = new Vue(
         el: `#app`,
         data: {
             mailList: [],
+            newMail : "",
         },
         methods : {
             printMailList(){
                 const self = this;
-                    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-                        .then(function (answer){
-                            const newMail = answer.data.response
-                            self.mailList.push(newMail)
-                            return newMail
-                        })
-                        console.warn(self.mailList)
+                    for (let i = 0 ; i< 10 ; i++){
+
+                        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                            .then(function (answer){
+                                const newMail = answer.data.response
+                                self.mailList.push(newMail)
+                                return self.newMail=newMail;
+                            })
+                    }
             }
         },
         // Updated per verifica dati e test funzioni
